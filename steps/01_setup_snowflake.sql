e5e61dfde5478a5fc835f1129e0a6990304fda52
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2025-01-21 08:40:16 +0000

    commit changes

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -14,14 +14,14 @@ CREATE OR ALTER DATABASE QUICKSTART_COMMON;
 -- API integration is needed for GitHub integration
 CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('https://github.com/<insert GitHub username>') -- INSERT YOUR GITHUB USERNAME HERE
+  API_ALLOWED_PREFIXES = ('https://github.com/Chinmay998') -- INSERT YOUR GITHUB USERNAME HERE
   ENABLED = TRUE;
 
 
 -- Git repository object is similar to external stage
 CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
   API_INTEGRATION = git_api_integration
-  ORIGIN = '<insert URL of forked GitHub repo>'; -- INSERT URL OF FORKED REPO HERE
+  ORIGIN = 'https://github.com/Chinmay998/sfguide-getting-started-with-snowflake-devops.git'; -- INSERT URL OF FORKED REPO HERE
 
 
 CREATE OR ALTER DATABASE QUICKSTART_PROD;
@@ -46,3 +46,6 @@ CREATE OR ALTER STAGE bronze.raw;
 
 -- Copy file from GitHub to internal stage
 copy files into @bronze.raw from @quickstart_common.public.quickstart_repo/branches/main/data/airport_list.json;
+
+
+execute immediate from @quickstart_common.public.quickstart_repo/branches/dev/steps/0[134]_*;
--- steps/05_database_change_management.sh
+++ steps/05_database_change_management.sh
@@ -67,6 +67,5 @@ Commit your changes to the "dev" branch before continuing
 '
 
 # Fetch changes from GitHub
-snow git fetch quickstart_common.public.quickstart_repo
 # Deploy the updated data pipeline
 snow git execute @quickstart_common.public.quickstart_repo/branches/dev/steps/0[134]_*
